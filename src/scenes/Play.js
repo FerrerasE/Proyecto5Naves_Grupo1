0486b07284b0bff31731b3b51ace5009b87fc81b
claudiagiselalima <143417057+claudiagiselalima@users.noreply.github.com>
2023-10-11 20:32:48 -0300

    nave y enemigos

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,5 +1,4 @@
 class Play extends Phaser.Scene {
-
     constructor() {
         super("Play");
         this.control01 = false;
@@ -7,16 +6,24 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('fondo', '../public/img/sky.png');
-        this.load.image('red', '../public/img/red.png');
-        this.load.spritesheet('nave', '../public/img/nave.png', { frameWidth: 70, frameHeight: 62 });
+        this.load.image('fondo', 'public/img/sky.png');
+        this.load.image('red', 'public/img/red.png');
+        this.load.spritesheet('nave', 'public/img/nave.png', { frameWidth: 70, frameHeight: 62 });
+        this.load.image('enemy', 'public/img/enemy.png');
     }
 
     create() {
         this.add.image(400, 300, 'fondo');
         this.player = this.physics.add.sprite(100, 30, 'nave');
+        this.enemigos = this.physics.add.group();
 
-        //this.player.setBounce(0.2);   cuidado 
+        this.time.addEvent({
+            delay: 1000,
+            callback: this.generarEnemigo,
+            callbackScope: this,
+            repeat: 5
+        });
+
         this.player.setCollideWorldBounds(true);
 
         this.anims.create({
@@ -58,35 +65,48 @@ class Play extends Phaser.Scene {
             blendMode: 'ADD'
         });
 
-
-
         particles.startFollow(this.player);
 
-
+        // Configura la colisión entre el jugador y los enemigos
+        this.physics.add.collider(this.player, this.enemigos, this.playerEnemy, null, this);
     }
 
     update() {
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
             this.player.anims.play('left', true);
-        }
-        else if (this.cursors.right.isDown) {
+        } else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160);
             this.player.anims.play('right', true);
-        }
-        else {
+        } else {
             this.player.setVelocityX(0);
             this.player.anims.play('turn');
         }
         if (this.cursors.up.isDown) {
             this.player.setVelocityY(-330);
             this.player.anims.play('up');
-        } else if (this.cursors.down.isDown) { this.player.setVelocityY(200);
-            this.player.anims.play('down'); }
-
-
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(200);
+            this.player.anims.play('down');
+        }
     }
 
-};
+    generarEnemigo() {
+        const enemigo = this.enemigos.create(
+            800,
+            Phaser.Math.Between(100, 500),
+            'enemy'
+        );
+
+        enemigo.setVelocityX(-100);
+    }
+
+    playerEnemy(player, enemy) {
+        // Cuando el jugador colisiona con un enemigo, finaliza el juego
+        this.scene.start('GameOver');
+    }
+}
+
 export default Play;
+
 
